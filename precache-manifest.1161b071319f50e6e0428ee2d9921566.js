be92832920355e0aa24d86244b30c7c793c42b3a
chemazu <chukwuemekachemazu@gmail.com>
2020-05-04 22:28:15 +0100

    Updates

---
+++ precache-manifest.1161b071319f50e6e0428ee2d9921566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3729eeb25b5994aa56f4f17dd333a308",
+    "url": "/Geetico/index.html"
+  },
+  {
+    "revision": "3ec1463241c1985ec4e5",
+    "url": "/Geetico/static/css/main.478b6ed1.chunk.css"
+  },
+  {
+    "revision": "cfb484f88d56be9e6f0e",
+    "url": "/Geetico/static/js/2.85f90f62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Geetico/static/js/2.85f90f62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec1463241c1985ec4e5",
+    "url": "/Geetico/static/js/main.8d54d747.chunk.js"
+  },
+  {
+    "revision": "b55eeefcbd9c160fb98e",
+    "url": "/Geetico/static/js/runtime-main.0471bccf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Geetico/precache-manifest.24a9657fc90f1bc69eb8b5b7a33caca3.js"
+  "/Geetico/precache-manifest.1161b071319f50e6e0428ee2d9921566.js"
 );
 
 self.addEventListener('message', (event) => {
