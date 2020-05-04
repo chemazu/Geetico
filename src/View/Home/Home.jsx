e55d65c415700016f045fde46008b54d963fae58
chemazu <chukwuemekachemazu@gmail.com>
2020-05-05 00:07:49 +0100

    header

--- src/View/Home/Home.jsx
+++ src/View/Home/Home.jsx
@@ -8,22 +8,26 @@ export class Home extends Component {
         return (
             <div>
                 <Header/>
+                <div className="somestuff">SOME STUFF</div>
                 <div className="headerSlider">
                     <div className="categories">
                         <ul>
-                            <li>One</li>
-                            <li>One</li>
-                            <li>One</li>
-                            <li>One</li>
-                            <li>One</li>
-                            <li>One</li>
-                            <li>One</li>
-                            <li>One</li>
-                            <li>One</li>
-                            <li>One</li>
+                            <li><a href="/">Top Sellers</a></li>
+                            <li><a href="/">New Arrivals</a></li>
+                            <li><a href="/">Baking Ingredients</a></li>
+                            <li><a href="/">Cake Decoration</a></li>
+                            <li><a href="/">Frosting & Design</a></li>
+                            <li><a href="/">Equipment & Tools</a></li>
+                            <li><a href="/">Cooking</a></li>
+                            <li><a href="/">Appliances</a></li>
                         </ul>
                     </div>
-                    <div className="slider">stuff</div>
+                    <div class="masthead responsive-image slider">
+   <header class="entry-header">
+      <h1 class="entry-title">Heading Spaced with padding</h1>
+      <h2 class="entry-subtitle">Some sort of lovely supporting title</h2>
+    </header>
+  </div>
                 </div>
             </div>
         )
